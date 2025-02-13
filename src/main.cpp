c2e078177b395e5ade3c9cba78a2fa86bae3273e
Toalha <rafaelpedrocarvalho@gmail.com>
2025-02-13 18:02:04 +0000

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -3,12 +3,14 @@
 enum commands {
   quit, // exit is the name of a function already
   echo,
+  type,
   unknown
 };
 
 commands identify_command(std::string str){
-  if(str.length() > 5 && !str.compare(0,5,"exit ")) return quit;
-  else if(str.length() > 5 && !str.compare(0,5,"echo ")) return echo;
+  if(str.length() >= 4 && !str.compare(0,4,"exit")) return quit;
+  else if(str.length() >= 4 && !str.compare(0,4,"echo")) return echo;
+  else if(str.length() >= 4 && !str.compare(0,4,"type")){return type;}
   else return unknown;
 }
 
@@ -40,6 +42,21 @@ int main() {
     
     case echo:
       std::cout << input.substr(5) << std::endl;
+      break;
+    
+    case type:
+      // Identifies what is the input after "type"
+      switch (identify_command(input.substr(5)))
+      { 
+      case unknown:
+        std::cout << input.substr(5) << ": not found" << std::endl;
+        break;
+
+      // If the command isn't unknown, then it is a shell builtin command
+      default:
+        std::cout << input.substr(5) << " is a shell builtin" << std::endl;
+        break;
+      }
       break;
     
     case unknown:
